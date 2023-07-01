bd5a528cf040b0087d6156d4a03dbc8a9867b729
Peter Kehl <peter.kehl@gmail.com>
2023-06-30 23:44:46 -0700

    Without tokio's "macros" feature.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use axum::{routing::get, Router};
+use axum::{routing, Router};
 use std::env;
 
-#[tokio::main]
-async fn main() {
+async fn tokio_main() {
     // Build our application with a single route.
-    let app = Router::new().route("/", get(|| async { "Hello, Space!" }));
+    let app = Router::new().route("/", routing::get(|| async { "Hello, Space!" }));
     // Get the port to listen on from the environment, or default to 8080 if not present.
     let addr = format!(
         "127.0.0.1:{}",
@@ -17,4 +16,18 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+// Result of `cargo expand` on code that used `#[tokio::main] async fn main() {...}`, which we've
+// replaced with `aync fn tokio_main()`.
+fn main() {
+    let body = tokio_main();
+    #[allow(clippy::expect_used, clippy::diverging_sub_expression)]
+    {
+        return tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .expect("Failed building the Runtime")
+            .block_on(body);
+    }
 }
